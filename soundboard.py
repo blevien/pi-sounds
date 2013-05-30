cc97df29693d173e9a878976d5af0373902e4a10
Bill Levien <blevien@safaribooksonline.com>
2013-05-30 11:49:14 -0700

    Added Second LED on GPIO.output(22) and Light Pattern to Button GPIO.input(25)

--- soundboard.py
+++ soundboard.py
@@ -3,25 +3,29 @@ import time
 import pygame.mixer 
 from sys import exit
 
+#configure Pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 GPIO.setup(18, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
+
+#Initialize variables
 x=0
-
 GPIO.output(17, False)
+GPIO.output(22, False)
 
+#initialize pygame mixer and sounds
 pygame.mixer.init(48000, -16, 1, 1024)
-
 sndA = pygame.mixer.Sound("sounds/siren2.wav")
 sndB = pygame.mixer.Sound("sounds/cddyshack.wav")
 sndC = pygame.mixer.Sound("sounds/train.wav")
-
 soundChannelA = pygame.mixer.Channel(1)
 soundChannelB = pygame.mixer.Channel(2)
 soundChannelC = pygame.mixer.Channel(3)
 
+#Loop through the program to continually test if buttons are high or low
 try:
     while True:
         
@@ -66,18 +70,24 @@ try:
 
 
         #This code is just for blinky lights while button is pressed
+        #Now with two alternating LEDs (Police Siren)
         input_value = GPIO.input(25)
         if input_value == False:
-            print('The light should be blinky.')
+            print('The light should blink, and sound should play until you release the button')
      	    soundChannelA.play(sndA)
             while input_value == False:
                 input_value = GPIO.input(25)
                 GPIO.output(17, True)
+                GPIO.output(22, False)
                 time.sleep(.1)
                 GPIO.output(17, False)
+                GPIO.output(22, True)
                 time.sleep(.1)
                 #print('Button 3 is still pressed.')
-            soundChannelA.stop()                
+            soundChannelA.stop()
+            GPIO.output(17, False)
+            GPIO.output(22, False)
+            print('Button released, lights and sound off')                
 
 
 
